e899e61ec8fff4426cf0ce63b11d2171e1462da6
Aaron W Morris <aaron@aarmor.net>
2021-08-03 10:18:23 -0400

    create pycurl_ftp transfer method

--- indi_timelapse/filetransfer/__init__.py
+++ indi_timelapse/filetransfer/__init__.py
@@ -3,6 +3,8 @@ from .python_ftp import python_ftp
 from .python_ftpes import python_ftpes
 from .pycurl_sftp import pycurl_sftp
 from .pycurl_sftp import sftp
+from .pycurl_ftp import pycurl_ftp
+from .pycurl_ftp import ftp
 from .pycurl_ftps import pycurl_ftps
 from .pycurl_ftps import ftps
 from .pycurl_ftpes import pycurl_ftpes
@@ -16,6 +18,7 @@ __all__ = (
     'pycurl_ftps',
     'pycurl_ftpes',
     'sftp',
+    'ftp',
     'ftps',
     'ftpes',
 )
