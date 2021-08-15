c0f7fa63beb48216ff4a79df373a0ded6ce81452
Aaron W Morris <aaron@aarmor.net>
2021-08-15 14:02:13 -0400

    gifbuilder is now fully commandline interactive

--- testing/gif.py
+++ testing/gif.py
@@ -1,21 +1,55 @@
 #!/usr/bin/env python3
 
 import imageio
+import argparse
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging
 
 
-filenames = (
-    '20210613_234008.jpg',
-    '20210613_234023.jpg',
-    '20210613_234038.jpg',
-    '20210613_234053.jpg',
-    '20210613_234108.jpg',
-    '20210613_234123.jpg',
-    '20210613_234138.jpg',
-    '20210613_234153.jpg',
-)
+class GifBuilder(object):
+    def __init__(self, duration=0.25):
+        self.duration = float(duration)
 
-with imageio.get_writer('movie.gif', mode='I', duration=0.2) as writer:
-    for filename in filenames:
-        image = imageio.imread(filename)
-        writer.append_data(image)
+
+    def main(self, outfile, inputfiles):
+        logger.warning('Creating %s', outfile)
+        with imageio.get_writer(outfile, mode='I', duration=self.duration) as writer:
+            for filename in inputfiles:
+                logger.info(' Reading %s', filename)
+                image = imageio.imread(filename)
+                writer.append_data(image)
+
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        'inputfiles',
+        help='Input files',
+        metavar='I',
+        type=str,
+        nargs='+'
+    )
+    argparser.add_argument(
+        '--output',
+        '-o',
+        help='output',
+        type=str,
+        required=True,
+    )
+    argparser.add_argument(
+        '--duration',
+        '-d',
+        help='duration',
+        type=float,
+        default=0.25,
+        required=False,
+    )
+
+    args = argparser.parse_args()
+
+    gb = GifBuilder(duration=args.duration)
+    gb.main(args.output, args.inputfiles)
 
