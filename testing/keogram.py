1e0b2cd62531e3e04d83ec2fb02e57954a0ed55e
Aaron W Morris <aaron@aarmor.net>
2021-08-21 18:12:04 -0400

    updates to keogram testing script

--- testing/keogram.py
+++ testing/keogram.py
@@ -2,13 +2,11 @@
 
 import cv2
 import numpy
-import sys
-import copy
 import argparse
 import logging
+import time
+from pathlib import Path
 from pprint import pformat
-
-ANGLE = 80
 
 
 logging.basicConfig(level=logging.INFO)
@@ -17,20 +15,38 @@ logger = logging
 
 
 class KeogramBuilder(object):
-    def __init__(self):
-        pass
+    def __init__(self, angle=0):
+        self.angle = angle
 
 
-    def main(self, outfile, inputfiles):
+    def main(self, outfile, inputdir):
+        file_list = list()
+        self.getFolderFilesByExt(inputdir, file_list)
+
+        # Exclude empty files
+        file_list_nonzero = filter(lambda p: p.stat().st_size != 0, file_list)
+
+        # Sort by timestamp
+        file_list_ordered = sorted(file_list_nonzero, key=lambda p: p.stat().st_mtime)
+
+
+
         # We do not know the array dimensions until the first image is rotated
-        keogram_data = False
+        keogram_data = None
 
-        for filename in inputfiles:
+        processing_start = time.time()
+
+        for filename in file_list_ordered:
             logger.info('Reading file: %s', filename)
-            image = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
+            image = cv2.imread(str(filename), cv2.IMREAD_UNCHANGED)
+
+            if type(image) is type(None):
+                logger.error('Unable to read %s', filename)
+                continue
+
             #logger.info('Data: %s', pformat(image))
 
-            rotated_image = self.rotate(image, ANGLE)
+            rotated_image = self.rotate(image)
             del image
 
 
@@ -42,7 +58,7 @@ class KeogramBuilder(object):
             #logger.info('Size: %s', pformat(rotated_center_line.size))
 
 
-            if type(keogram_data) is bool:
+            if type(keogram_data) is type(None):
                 new_shape = rotated_center_line.shape
                 logger.info('New Shape: %s', pformat(new_shape))
 
@@ -54,18 +70,21 @@ class KeogramBuilder(object):
             keogram_data = numpy.append(keogram_data, rotated_center_line, 1)
 
 
+        processing_elapsed_s = time.time() - processing_start
+        logger.info('Images processed in %0.1f s', processing_elapsed_s)
+
         #logger.info('Data: %s', pformat(keogram_data))
 
         logger.warning('Creating %s', outfile)
         cv2.imwrite(outfile, keogram_data, [cv2.IMWRITE_JPEG_QUALITY, 90])
 
 
-    def rotate(self, image, angle):
+    def rotate(self, image):
             height, width = image.shape[:2]
             center = (width/2, height/2)
 
-            rot = cv2.getRotationMatrix2D(center, ANGLE, 1.0)
-            #bbox = cv2.boundingRect2f((0, 0), image.size(), ANGLE)
+            rot = cv2.getRotationMatrix2D(center, self.angle, 1.0)
+            #bbox = cv2.boundingRect2f((0, 0), image.size(), self.angle)
 
             #rot[0, 2] += bbox.width/2.0 - image.cols/2.0
             #rot[1, 2] += bbox.height/2.0 - imagesrc.rows/2.0
@@ -85,16 +104,30 @@ class KeogramBuilder(object):
             return rotated
 
 
+    def getFolderFilesByExt(self, folder, file_list, extension_list=None):
+        if not extension_list:
+            extension_list = ['jpg']
+
+        logger.info('Searching for image files in %s', folder)
+
+        dot_extension_list = ['.{0:s}'.format(e) for e in extension_list]
+
+        for item in Path(folder).iterdir():
+            if item.is_file() and item.suffix in dot_extension_list:
+                file_list.append(item)
+            elif item.is_dir():
+                self.getFolderFilesByExt(item, file_list, extension_list=extension_list)  # recursion
+
+
+
 
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
-        'inputfiles',
-        help='Input files',
-        metavar='I',
+        'inputdir',
+        help='Input directory',
         type=str,
-        nargs='+'
     )
     argparser.add_argument(
         '--output',
@@ -103,9 +136,17 @@ if __name__ == "__main__":
         type=str,
         required=True,
     )
+    argparser.add_argument(
+        '--angle',
+        '-a',
+        help='angle',
+        type=int,
+        default=45,
+    )
+
 
     args = argparser.parse_args()
 
-    kb = KeogramBuilder()
-    kb.main(args.output, args.inputfiles)
+    kb = KeogramBuilder(angle=args.angle)
+    kb.main(args.output, args.inputdir)
 
