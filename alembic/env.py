b7b805241ed9e2a84df4ae1d316e8b9e0cf3f093
Aaron W Morris <aaron@aarmor.net>
2021-09-14 19:23:04 -0400

    support auto migrations with alembic

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,12 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from indi_allsky.db.models import Base
+from indi_allsky.db.models import IndiAllSkyDbCameraTable
+from indi_allsky.db.models import IndiAllSkyDbImageTable
+from indi_allsky.db.models import IndiAllSkyDbVideoTable
+from indi_allsky.db.models import IndiAllSkyDbKeogramTable
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -17,7 +23,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
