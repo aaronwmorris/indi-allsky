df0ea78caea9f9de833177f89a1d0a0429201b48
Aaron W Morris <aaron@aarmor.net>
2021-09-15 21:26:11 -0400

    missed alembic metadata config

--- alembic/env.py
+++ alembic/env.py
@@ -23,7 +23,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
