8985c43a79f7bf604629f50236e6e31c7a0b2b8b
Aaron W Morris <aaron@aarmor.net>
2021-12-27 11:54:03 -0500

    update startrails testing code

--- testing/startrails.py
+++ testing/startrails.py
@@ -27,6 +27,8 @@ class StartrailGenerator(object):
         self.background_image_brightness = 255
         self.background_image_min_brightness = 10
 
+        self.image_processing_elapsed_s = 0
+
 
     def main(self, outfile, inputdir):
         file_list = list()
@@ -49,51 +51,70 @@ class StartrailGenerator(object):
                 logger.error('Unable to read %s', filename)
                 continue
 
-
-            if isinstance(self.trail_image, type(None)):
-                image_height, image_width = image.shape[:2]
-
-                # base image is just a black image
-                if len(image.shape) == 2:
-                    self.trail_image = numpy.zeros((image_height, image_width), dtype=numpy.uint8)
-                else:
-                    self.trail_image = numpy.zeros((image_height, image_width, 3), dtype=numpy.uint8)
+            self.processImage(image)
 
 
-            # need grayscale image for mask generation
+        self.finalize(outfile)
+
+
+        logger.warning('Images processed in %0.1f s', self.image_processing_elapsed_s)
+
+
+        processing_elapsed_s = time.time() - processing_start
+        logger.warning('Total processing in %0.1f s', processing_elapsed_s)
+
+
+    def processImage(self, image):
+
+        image_processing_start = time.time()
+
+        if isinstance(self.trail_image, type(None)):
+            image_height, image_width = image.shape[:2]
+
+            # base image is just a black image
             if len(image.shape) == 2:
-                image_gray = image.copy()
+                self.trail_image = numpy.zeros((image_height, image_width), dtype=numpy.uint8)
             else:
-                image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-            m_avg = cv2.mean(image_gray)[0]
-            if m_avg > self.max_brightness:
-                logger.warning(' Excluding image due to brightness: %0.2f', m_avg)
-                continue
-
-            #logger.info(' Image brightness: %0.2f', m_avg)
-
-            if m_avg < self.background_image_brightness and m_avg > self.background_image_min_brightness:
-                # try to exclude images that are too dark
-                logger.info('Found new background candidate: %s, score %0.2f', filename, m_avg)
-                self.background_image_brightness = m_avg  # new low score
-                self.background_image = image  # image with the lowest score will be the permanent background
+                self.trail_image = numpy.zeros((image_height, image_width, 3), dtype=numpy.uint8)
 
 
-            ret, mask = cv2.threshold(image_gray, 127, 255, cv2.THRESH_BINARY)
-            mask_inv = cv2.bitwise_not(mask)
+        # need grayscale image for mask generation
+        if len(image.shape) == 2:
+            image_gray = image.copy()
+        else:
+            image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        m_avg = cv2.mean(image_gray)[0]
+        if m_avg > self.max_brightness:
+            logger.warning(' Excluding image due to brightness: %0.2f', m_avg)
+            return
+
+        #logger.info(' Image brightness: %0.2f', m_avg)
+
+        if m_avg < self.background_image_brightness and m_avg > self.background_image_min_brightness:
+            # try to exclude images that are too dark
+            logger.info('Found new background candidate: score %0.2f', m_avg)
+            self.background_image_brightness = m_avg  # new low score
+            self.background_image = image  # image with the lowest score will be the permanent background
 
 
-            # Now black-out the area of stars in the background
-            bg_masked = cv2.bitwise_and(self.trail_image, self.trail_image, mask=mask_inv)
-
-            # Take only stars of original image
-            stars_masked = cv2.bitwise_and(image, image, mask=mask)
-
-            # Put stars on background
-            self.trail_image = cv2.add(bg_masked, stars_masked)
+        ret, mask = cv2.threshold(image_gray, 127, 255, cv2.THRESH_BINARY)
+        mask_inv = cv2.bitwise_not(mask)
 
 
+        # Now black-out the area of stars in the background
+        bg_masked = cv2.bitwise_and(self.trail_image, self.trail_image, mask=mask_inv)
+
+        # Take only stars of original image
+        stars_masked = cv2.bitwise_and(image, image, mask=mask)
+
+        # Put stars on background
+        self.trail_image = cv2.add(bg_masked, stars_masked)
+
+        self.image_processing_elapsed_s += time.time() - image_processing_start
+
+
+    def finalize(self, outfile):
         # need grayscale image for mask generation
         if len(self.trail_image.shape) == 2:
             base_image_gray = self.trail_image.copy()
@@ -113,10 +134,6 @@ class StartrailGenerator(object):
 
         # Put stars on background
         final_image = cv2.add(bg_masked, stars_masked)
-
-
-        processing_elapsed_s = time.time() - processing_start
-        logger.info('Images processed in %0.1f s', processing_elapsed_s)
 
 
         logger.warning('Creating %s', outfile)
