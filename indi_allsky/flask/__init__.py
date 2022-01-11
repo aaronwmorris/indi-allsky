5ab7a636a0884132fd6ad0ab6347103f671b6137
Aaron W Morris <aaron@aarmor.net>
2022-01-11 16:15:34 -0500

    add logging config

--- indi_allsky/flask/__init__.py
+++ indi_allsky/flask/__init__.py
@@ -1,4 +1,6 @@
 import json
+from logging.config import dictConfig
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,6 +11,27 @@ migrate = Migrate()
 csrf = CSRFProtect()
 
 from .views import bp  # noqa
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        },
+    },
+    'handlers': {
+        'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    },
+})
 
 
 def create_app():
