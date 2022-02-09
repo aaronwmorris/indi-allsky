eed05c0ad4f3cba043d9d4013ec5fe613f16b0cf
Aaron W Morris <aaron@aarmor.net>
2022-02-09 09:11:22 -0500

    pyindi-client rebuild script updates

--- rebuild_pyindi-client.sh
+++ rebuild_pyindi-client.sh
@@ -8,6 +8,15 @@ PATH=/bin:/usr/bin
 export PATH
 
 
+if [[ "$(id -u)" == "0" ]]; then
+    echo "Please do not run $(basename $0) as root"
+    echo "Re-run this script as the user which will execute the indi-allsky software"
+    echo
+    echo
+    exit 1
+fi
+
+
 # find script directory for service setup
 SCRIPT_DIR=$(dirname $0)
 cd "$SCRIPT_DIR"
@@ -15,7 +24,26 @@ ALLSKY_DIRECTORY=$PWD
 cd $OLDPWD
 
 
+if [ ! -d "${ALLSKY_DIRECTORY}/virtualenv/indi-allsky" ]; then
+    echo
+    echo "indi-allsky virtualenv not found, please run setup.sh"
+    echo
+    echo
+    exit 1
+fi
+
+
+echo
+echo
+echo "The purpose of this script is to rebuild pyindi-client against"
+echo " newer indilib packages."
+echo
+echo "Rebuild proceeding in 10 seconds... (control-c to cancel)"
+echo
+sleep 10
+
+
 source ${ALLSKY_DIRECTORY}/virtualenv/indi-allsky/bin/activate
 pip3 uninstall -y pyindi-client
-pip3 install --no-binary :all: pyindi-client
+pip3 install --no-binary :all: --upgrade pyindi-client
 
