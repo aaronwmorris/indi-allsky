c43f943cf6e2cf28a9c5925c4dfb4d2e4f5b773c
Aaron W Morris <aaron@aarmor.net>
2022-06-28 20:34:36 -0400

    add navigation for public view

--- indi_allsky/flask/views.py
+++ indi_allsky/flask/views.py
@@ -1962,7 +1962,8 @@ bp.add_url_rule('/system', view_func=SystemInfoView.as_view('system_view', templ
 bp.add_url_rule('/tasks', view_func=TaskQueueView.as_view('taskqueue_view', template_name='taskqueue.html'))
 bp.add_url_rule('/timelapse', view_func=TimelapseGeneratorView.as_view('timelapse_view', template_name='timelapse.html'))
 
-bp.add_url_rule('/public', view_func=IndexView.as_view('public_view', template_name='public.html'))
+bp.add_url_rule('/public', view_func=IndexView.as_view('public_index_view', template_name='public_index.html'))
+bp.add_url_rule('/public/loop', view_func=ImageLoopView.as_view('public_image_loop_view', template_name='public_loop.html'))
 
 bp.add_url_rule('/ajax/imageviewer', view_func=AjaxImageViewerView.as_view('ajax_imageviewer_view'))
 bp.add_url_rule('/ajax/videoviewer', view_func=AjaxVideoViewerView.as_view('ajax_videoviewer_view'))
