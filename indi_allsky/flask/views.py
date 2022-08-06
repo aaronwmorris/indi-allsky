a6b703af106f650e120dc79cd36ff0570991fae6
Aaron W Morris <aaron@aarmor.net>
2022-08-06 14:54:19 -0400

    use 32bit float instead of 64bit for laplacian for speed

--- indi_allsky/flask/views.py
+++ indi_allsky/flask/views.py
@@ -2295,7 +2295,7 @@ class JsonFocusView(JsonView):
         vl_start = time.time()
 
         ### determine variance of laplacian
-        blur_score = cv2.Laplacian(image_data, cv2.CV_64F).var()
+        blur_score = cv2.Laplacian(image_data, cv2.CV_32F).var()
         json_data['blur_score'] = float(blur_score)
 
         vl_elapsed_s = time.time() - vl_start
