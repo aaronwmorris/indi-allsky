0e2e20bf6c8109595b3fd04bb0c25cd59658e7b6
Aaron W Morris <aaron@aarmor.net>
2022-08-15 19:30:17 -0400

    working on libcamera class

--- indi_allsky/camera/libcamera.py
+++ indi_allsky/camera/libcamera.py
@@ -1,3 +1,7 @@
+import tempfile
+import subprocess
+import psutil
+from pathlib import Path
 import logging
 
 from .fake_indi import FakeIndiClient
@@ -12,14 +16,59 @@ class FakeIndiLibCamera(FakeIndiClient):
     def __init__(self, *args, **kwargs):
         super(FakeIndiLibCamera, self).__init__(*args, **kwargs)
 
+        self.libcamera_pid = None
+
+        self.max_gain = 16
+        self.min_gain = 1
+
 
     def setCcdExposure(self, exposure, sync=False, timeout=None):
-        pass
+        if self._libCameraPidRunning():
+            return False
+
+        image_tmp_f = tempfile.NamedTemporaryFile(mode='w', suffix='.dng', delete=False)
+        image_tmp_p = Path(image_tmp_f.name)
+        image_tmp_f.close()
+
+        exposure_us = int(exposure * 1000000)
+
+        cmd = [
+            'libcamera-still',
+            '--immediate',
+            '--raw',
+            '--awbgains', '1.0,1.0,1.0',
+            '--gain', '{0:d}'.format(self._ccd_gain),
+            '--shutter', '{0:d}'.format(exposure_us),
+            '--output', str(image_tmp_p),
+        ]
+
+        logger.info('image command: %s', cmd.join(' '))
+
+        libcamera_subproc = subprocess.Popen(
+            cmd,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+        self.libcamera_pid = libcamera_subproc.pid
 
 
     def getCcdExposureStatus(self):
         # returns camera_ready, exposure_state
-        pass
+        if not self._libCameraPidRunning():
+            return False, 'BUSY'
+
+        return True, 'READY'
+
+
+    def _libCameraPidRunning(self):
+        if not self.libcamera_pid:
+            return False
+
+        if psutil.pid_exists(self.libcamera_pid):
+            return True
+
+        return False
 
 
     def findCcd(self):
