1b9b01e310ec49290e06729035a9c681703e8f49
Aaron W Morris <aaron@aarmor.net>
2022-10-25 11:28:11 -0400

    lunar phase not necessary for eclipse calculations

--- testing/sun_moon_sep.py
+++ testing/sun_moon_sep.py
@@ -34,13 +34,27 @@ for x in range(20000):
     sun.compute(obs)
     moon.compute(obs)
 
+    sun_alt = math.degrees(sun.alt)
+
     moon_phase = moon.moon_phase * 100.0
 
     # separation of 1-3 degrees means a possible eclipse
     sun_moon_sep = abs((ephem.separation(moon, sun) / (math.pi / 180)) - 180)
 
-    if sun_moon_sep < 0.8: 
-        logger.info('Date: %s, separation: %0.3f, phase %0.2f', utcnow, sun_moon_sep, moon_phase)
+    if sun_moon_sep < 0.8:
+        # Lunar
+
+        if sun_alt > -6:
+            # not night time
+            continue
+
+        logger.info('Lunar: %s, separation: %0.3f, phase %0.2f', utcnow, sun_moon_sep, moon_phase)
     elif sun_moon_sep > 179.5:
-        logger.info('Date: %s, separation: %0.3f, phase %0.2f', utcnow, sun_moon_sep, moon_phase)
+        # Solar
+
+        if sun_alt < -6:
+            # not day time
+            continue
+
+        logger.info('Solar: %s, separation: %0.3f, phase %0.2f', utcnow, sun_moon_sep, moon_phase)
 
