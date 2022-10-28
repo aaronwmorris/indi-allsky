2a61e38384192d0b1e203b0bb03759e234fae9e9
Aaron W Morris <aaron@aarmor.net>
2022-10-28 19:56:35 -0400

    flake8 update

--- indi_allsky/filetransfer/exceptions.py
+++ indi_allsky/filetransfer/exceptions.py
@@ -1,14 +1,18 @@
 class AuthenticationFailure(Exception):
     pass
 
+
 class ConnectionFailure(Exception):
     pass
+
 
 class TransferFailure(Exception):
     pass
 
+
 class PermissionFailure(Exception):
     pass
+
 
 class CertificateValidationFailure(Exception):
     pass
