c4e249f636ff5af53481ac2ed2581319f708b5e1
Aaron W Morris <aaron@aarmor.net>
2022-12-03 13:02:52 -0500

    better indexes

--- misc/perf_indexes.sql
+++ misc/perf_indexes.sql
@@ -1,7 +1,12 @@
-CREATE INDEX idx_image_createDate_Y on image (CAST(STRFTIME("%Y", "createDate") AS INTEGER));
-CREATE INDEX idx_image_createDate_m on image (CAST(STRFTIME("%m", "createDate") AS INTEGER));
-CREATE INDEX idx_image_createDate_d on image (CAST(STRFTIME("%d", "createDate") AS INTEGER));
-CREATE INDEX idx_image_createDate_H on image (CAST(STRFTIME("%H", "createDate") AS INTEGER));
+CREATE INDEX idx_image_createDate_YmdH on image (
+ CAST(STRFTIME("%Y", "createDate") AS INTEGER),
+ CAST(STRFTIME("%m", "createDate") AS INTEGER),
+ CAST(STRFTIME("%d", "createDate") AS INTEGER),
+ CAST(STRFTIME("%H", "createDate") AS INTEGER)
+);
 
-CREATE INDEX idx_video_dayDate_Y on video (CAST(STRFTIME("%Y", "dayDate") AS INTEGER));
-CREATE INDEX idx_video_dayDate_m on video (CAST(STRFTIME("%m", "dayDate") AS INTEGER));
+
+CREATE INDEX idx_video_dayDate_Ym on video (
+ CAST(STRFTIME("%Y", "dayDate") AS INTEGER),
+ CAST(STRFTIME("%m", "dayDate") AS INTEGER)
+);
