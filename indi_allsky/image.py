8a34b3a663ffc6c9d7cda6ecfc1a0691565ab005
Aaron W Morris <aaron@aarmor.net>
2022-12-15 23:47:37 -0500

    working on average

--- indi_allsky/image.py
+++ indi_allsky/image.py
@@ -2063,7 +2063,7 @@ class ImageProcessor(object):
 class ImageStacker(object):
 
     def average(self, stack_data, numpy_type):
-        avg_image = numpy.mean(stack_data, axis=0)
+        avg_image = sum(stack_data) / len(stack_data)
         return numpy.floor(avg_image).astype(numpy_type)  # no floats
 
 
