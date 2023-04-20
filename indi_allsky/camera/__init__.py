635db45983eb5ac07115dffe62e59d9440e62a58
Aaron W Morris <aaron@aarmor.net>
2023-04-20 11:41:23 -0400

    add libcamera imx219 support

--- indi_allsky/camera/__init__.py
+++ indi_allsky/camera/__init__.py
@@ -2,6 +2,7 @@ from .indi import IndiClient as indi
 from .indi_passive import IndiClientPassive as indi_passive
 from .libcamera import IndiClientLibCameraImx477 as libcamera_imx477
 from .libcamera import IndiClientLibCameraImx378 as libcamera_imx378
+from .libcamera import IndiClientLibCameraImx219 as libcamera_imx219
 from .libcamera import IndiClientLibCameraImx519 as libcamera_imx519
 from .libcamera import IndiClientLibCamera64mpHawkeye as libcamera_64mp_hawkeye
 from .libcamera import IndiClientLibCameraImx708 as libcamera_imx708
@@ -13,6 +14,7 @@ __all__ = (
     'indi_passive',
     'libcamera_imx477',
     'libcamera_imx378',
+    'libcamera_imx219',
     'libcamera_imx519',
     'libcamera_64mp_hawkeye',
     'libcamera_imx708',
