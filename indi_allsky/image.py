9f6cf241c4d395a419313f0dfb2b32a8a9e9e501
Aaron W Morris <aaron@aarmor.net>
2023-04-26 14:47:35 -0400

    increase outline stroke

--- indi_allsky/image.py
+++ indi_allsky/image.py
@@ -3066,7 +3066,7 @@ class ImageProcessor(object):
     def drawText_pillow(self, draw, text, font, pt, color_rgb):
         if self.config['TEXT_PROPERTIES']['FONT_OUTLINE']:
             # black outline
-            stroke_width = 1
+            stroke_width = 2
         else:
             stroke_width = 0
 
