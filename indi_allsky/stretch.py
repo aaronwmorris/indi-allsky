2c2770f1c44f9d0de4252bf1ca01dd2d7be950e6
Aaron W Morris <aaron@aarmor.net>
2023-05-01 21:14:43 -0400

    remove debug option

--- indi_allsky/stretch.py
+++ indi_allsky/stretch.py
@@ -22,7 +22,7 @@ class IndiAllSkyStretch(object):
             return data
 
 
-        if self.config.get('IMAGE_STRETCH', {}).get('MODE1_ENABLE', True):
+        if self.config.get('IMAGE_STRETCH', {}).get('MODE1_ENABLE'):
             logger.info('Using stretch mode 1')
             return self.mode1_stretch(data, image_bit_depth)
         else:
