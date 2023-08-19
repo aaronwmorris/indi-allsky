b6d2706ae1b1f6e920536464c1aaa21ad3dc8339
Aaron W Morris <aaron@aarmor.net>
2023-08-19 19:08:35 -0400

    better artifact control

--- indi_allsky/image.py
+++ indi_allsky/image.py
@@ -2607,10 +2607,10 @@ class ImageProcessor(object):
 
         # clahe only accepts uint8 and uint16
         # luminance is a float between 0-100, which needs to be remapped ot a 16bit int
-        cl_u16 = clahe.apply((lab[:, :, 0] * 650).astype(numpy_dtype))  # a little less than 65535 / 100
+        cl_u16 = clahe.apply((lab[:, :, 0] * 655).astype(numpy_dtype))  # a little less than 65535 / 100
 
         # map luminance back to 0-100
-        lab[:, :, 0] = (cl_u16 / 656).astype(numpy.float32)  # trying to prevent artifiacts
+        lab[:, :, 0] = (cl_u16 / 690).astype(numpy.float32)  # 690 to prevent artifiacts in bright areas
 
         # convert back to uint8 or uint16
         self.image = (cv2.cvtColor(lab, cv2.COLOR_LAB2BGR) * max_value).astype(numpy_dtype)
