e94db7f35391450e42fd75bf3511ad972e5d8052
Aaron W Morris <aaron@aarmor.net>
2024-06-06 22:24:37 -0500

    add skyfield benchmark

--- testing/sat_tle_bench.py
+++ testing/sat_tle_bench.py
@@ -16,20 +16,22 @@ class SatBench(object):
         pass
 
     def main(self):
-        setup_sat = '''
+        setup_pyephem = '''
 from datetime import datetime
 from datetime import timezone
 import io
 import math
 import ephem
 
-obs = ephem.city('Atlanta')
+obs = ephem.Observer()
+obs.lat = math.radians(33.0)
+obs.lon = math.radians(-84.0)
 
 with io.open('/tmp/iss_27272897.txt', 'r') as f_tle:
     tle_data = f_tle.readlines()
 '''
 
-        s_sat = '''
+        s_pyephem = '''
 iss = ephem.readtle(*tle_data)
 obs.date = datetime.now(tz=timezone.utc)
 iss.compute(obs)
@@ -42,8 +44,40 @@ iss_next_pass = obs.next_pass(iss)
 '''
 
 
-        t_sat = timeit.timeit(stmt=s_sat, setup=setup_sat, number=self.rounds)
-        logger.info('Satellite calc: %0.3fms', t_sat * 1000 / self.rounds)
+        setup_skyfield = '''
+import io
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
+
+from skyfield.api import load
+from skyfield.api import wgs84
+from skyfield.api import EarthSatellite
+
+ts = load.timescale()
+
+location = wgs84.latlon(33.0, -84.0)
+
+with io.open('/tmp/iss_27272897.txt', 'r') as f_tle:
+    tle_data = f_tle.readlines()
+'''
+
+        s_skyfield = '''
+satellite = EarthSatellite(tle_data[1], tle_data[2], tle_data[0], ts)
+
+now = datetime.now(tz=timezone.utc)
+t0 = ts.from_datetime(now)
+t1 = ts.from_datetime(now + timedelta(hours=24))
+
+t, events = satellite.find_events(location, t0, t1, altitude_degrees=0.0)
+'''
+
+
+        t_pyephem = timeit.timeit(stmt=s_pyephem, setup=setup_pyephem, number=self.rounds)
+        logger.info('PyEphem calc: %0.3fms', t_pyephem * 1000 / self.rounds)
+
+        t_skyfield = timeit.timeit(stmt=s_skyfield, setup=setup_skyfield, number=self.rounds)
+        logger.info('Skyfield calc: %0.3fms', t_skyfield * 1000 / self.rounds)
 
 
 
