3ae04178e0e4d570fb81ed6cc390733750e7ad30
Aaron W Morris <aaron@aarmor.net>
2024-06-10 12:46:19 -0500

    additional info in setup.sh

--- setup.sh
+++ setup.sh
@@ -274,8 +274,8 @@ echo
 echo
 echo "indi-allsky supports the following camera interfaces."
 echo
-echo "          indi: For astro/planetary cameras normally connected via USB"
-echo "     libcamera: Supports cameras connected via CSI interface on Raspberry Pi SoCs"
+echo "          indi: For astro/planetary cameras normally connected via USB (ZWO, QHY, PlayerOne, SVBony, Altair, Touptek, etc)"
+echo "     libcamera: Supports cameras connected via CSI interface on Raspberry Pi SBCs (Raspi HQ Camera, Camera Module 3, etc)"
 echo "  indi_passive: Connect a second instance of indi-allsky to an existing indi-allsky indiserver"
 echo " pycurl_camera: Download images from a remote web camera"
 echo
