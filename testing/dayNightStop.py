7f01cc1b4732fb01b1b9ed5e78b6ea89bd2dc26f
Aaron W Morris <aaron@aarmor.net>
2024-06-16 20:48:32 -0500

    update day/night calculations

--- testing/dayNightStop.py
+++ testing/dayNightStop.py
@@ -17,8 +17,8 @@ logger = logging
 LATITUDE  = 33.0
 LONGITUDE = -84.0
 
-LATITUDE  = 62.9
-LONGITUDE = -160.0
+#LATITUDE  = 62.9
+#LONGITUDE = -160.0
 
 SUN_ALT   = -6.0
 
@@ -104,15 +104,15 @@ class DayNightStop(object):
 
         logger.info('Latitude:        %0.1f', LATITUDE)
         logger.info('Longitude:       %0.1f', LONGITUDE)
-        logger.info('Now:             %s, %0.1f', now, now_sun_alt)
+        logger.info('Now:             %s, %0.1f', now.strftime('%Y-%m-%d %H:%M:%S'), now_sun_alt)
         logger.info('Night:           %s', str(night))
-        logger.info('Start Day:       %s', start_day)
+        logger.info('Start Day:       %s', start_day.strftime('%Y-%m-%d %H:%M:%S'))
         #logger.info('Start Day UTC:   %s', start_day_utc)
         logger.info('UTC Offset:      %s', utc_offset)
         logger.info('Current dayDate  %s', dayDate)
-        logger.info('Today Transit:   %s', today_transit + utc_offset)
-        logger.info('Night Hard Stop: %s, %0.1f', night_stop + utc_offset, end_night_alt)
-        logger.info('Day Hard Stop:   %s, %0.1f', day_stop + utc_offset, end_day_alt)
+        logger.info('Today Transit:   %s', (today_transit + utc_offset).strftime('%Y-%m-%d %H:%M:%S'))
+        logger.info('Night Hard Stop: %s, %0.1f', (night_stop + utc_offset).strftime('%Y-%m-%d %H:%M:%S'), end_night_alt)
+        logger.info('Day Hard Stop:   %s, %0.1f', (day_stop + utc_offset).strftime('%Y-%m-%d %H:%M:%S'), end_day_alt)
 
 
 
