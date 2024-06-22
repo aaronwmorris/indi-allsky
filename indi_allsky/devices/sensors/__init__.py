242e5acf962ace0fb123ce9cc5293e041be138a0
Aaron W Morris <aaron@aarmor.net>
2024-06-22 14:47:54 -0500

    add TMP36 support via ads1x15 ADC

--- indi_allsky/devices/sensors/__init__.py
+++ indi_allsky/devices/sensors/__init__.py
@@ -23,9 +23,12 @@ from .lightSensorTsl2591 import LightSensorTsl2591_I2C as blinka_light_sensor_ts
 
 from .lightSensorBh1750 import LightSensorBh1750_I2C as blinka_light_sensor_bh1750_i2c
 
-from .tempSensorDs18x20 import TempSensorDs18x20 as kernel_temp_sensor_ds18x20_w1
+from .tempSensorTmp36_Ads1x15 import TempSensorTmp36_Ads1015_I2C as cpads_temp_sensor_tmp36_ads1015_i2c
+from .tempSensorTmp36_Ads1x15 import TempSensorTmp36_Ads1115_I2C as cpads_temp_sensor_tmp36_ads1115_i2c
 
 from .tempApiOpenWeatherMap import TempApiOpenWeatherMap as temp_api_openweathermap
+
+from .tempSensorDs18x20 import TempSensorDs18x20 as kernel_temp_sensor_ds18x20_w1
 
 
 __all__ = (
@@ -44,6 +47,8 @@ __all__ = (
     'blinka_light_sensor_tsl2561_i2c',
     'blinka_light_sensor_tsl2591_i2c',
     'blinka_light_sensor_bh1750_i2c',
+    'cpads_temp_sensor_tmp36_ads1015_i2c',
+    'cpads_temp_sensor_tmp36_ads1115_i2c',
     'kernel_temp_sensor_ds18x20_w1',
     'temp_api_openweathermap',
 )
