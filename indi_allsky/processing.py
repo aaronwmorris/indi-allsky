5cfef76cd315afd1e4882c71cc09bb7023c011f0
Aaron W Morris <aaron@aarmor.net>
2024-08-13 13:23:31 -0500

    use numpy.subtract() instead of cv2.subtract()

--- indi_allsky/processing.py
+++ indi_allsky/processing.py
@@ -688,8 +688,13 @@ class ImageProcessor(object):
                     black_level_scaled = int(libcamera_black_level) >> (16 - self.max_bit_depth)
 
                     # use opencv to prevent underruns
-                    i_ref['hdulist'][0].data = cv2.subtract(i_ref['hdulist'][0].data, black_level_scaled)
+                    #i_ref['hdulist'][0].data = cv2.subtract(i_ref['hdulist'][0].data, black_level_scaled)
                     #i_ref['hdulist'][0].data -= (black_level_scaled - 15)  # offset slightly
+
+                    i_ref['hdulist'][0].data = numpy.subtract(i_ref['hdulist'][0].data, black_level_scaled)
+
+                    # cutoff values below 0
+                    i_ref['hdulist'][0].data[i_ref['hdulist'][0].data < 0] = 0
 
                     i_ref['calibrated'] = True
 
@@ -838,7 +843,11 @@ class ImageProcessor(object):
             raise CalibrationNotFound('Dark frame calibration dimension mismatch')
 
 
-        data_calibrated = cv2.subtract(data, master_dark)
+        #data_calibrated = cv2.subtract(data, master_dark)
+        data_calibrated = numpy.subtract(data, master_dark)
+
+        # cutoff values less than 0
+        data_calibrated[data_calibrated < 0] = 0
 
         return data_calibrated
 
