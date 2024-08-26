4f91195f6ae9024d320e7894a99ee92765af7b68
Aaron W Morris <aaron@aarmor.net>
2024-08-26 15:32:51 -0500

    add init

---
+++ indi_allsky/stretch/__init__.py
@@ -0,0 +1,8 @@
+from .mode1 import IndiAllSky_Mode1_Stretch as stretch_mode1_stddev_cutoff
+from .mode2_mtf import IndiAllSky_Mode2_MTF_Stretch as stretch_mode2_mtf
+
+
+__all__ = (
+    'stretch_mode1_stddev_cutoff',
+    'stretch_mode2_mtf',
+)
