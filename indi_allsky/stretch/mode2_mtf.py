3c6211a8e5b69e486e3a51dd22742bc8140a7e64
Aaron W Morris <aaron@aarmor.net>
2024-08-26 20:30:51 -0500

    MTF is working

--- indi_allsky/stretch/mode2_mtf.py
+++ indi_allsky/stretch/mode2_mtf.py
@@ -33,32 +33,40 @@ class IndiAllSky_Mode2_MTF_Stretch(IndiAllSky_Stretch_Base):
 
 
     def stretch(self, data, image_bit_depth):
+        #logger.info('MTF: Shadows - Shadows %0.2f, Midtones %0.2f, Highlights %0.2f', self.shadows, self.midtones, self.highlights)
 
         mtf_start = time.time()
 
 
-        if image_bit_depth == 8:
-            numpy_dtype = numpy.uint8
-        else:
-            numpy_dtype = numpy.uint16
-
-        data_max = (2 ** image_bit_depth) - 1
-
-
         if isinstance(self._mtf_lut, type(None)):
             # only need to generate the lookup table once
+            if image_bit_depth == 8:
+                numpy_dtype = numpy.uint8
+            else:
+                numpy_dtype = numpy.uint16
+
+
+            data_max = (2 ** image_bit_depth) - 1
+
+
             range_array = numpy.arange(0, data_max, dtype=numpy.float32)
+            shadows_val = int(self.shadows * data_max)
+            highlights_val = int(self.highlights * data_max)
 
-
-            lut = (range_array - self.shadows) / (self.highlights - self.shadows)
-
+            # these will result in 1.0 normalized values
+            lut = (range_array - shadows_val) / (highlights_val - shadows_val)
             lut = ((self.midtones - 1) * lut) / (((2 * self.midtones - 1) * lut) - self.midtones)
+
+            # back to real values
+            lut = lut * data_max
 
 
             lut[lut < 0] = 0  # clip low end
             lut[lut > data_max] = data_max  # clip high end
 
             lut = lut.astype(numpy_dtype)  # this must come after clipping
+
+            #logger.info('Min: %d, Max: %d', numpy.min(lut), numpy.max(lut))
 
             self._mtf_lut = lut
 
