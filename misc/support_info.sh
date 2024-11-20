107a391ae1b2d80d719a75a428f0cdcb6375358f
Aaron W Morris <aaron@aarmor.net>
2024-11-20 10:31:56 -0600

    error checking for support info script

--- misc/support_info.sh
+++ misc/support_info.sh
@@ -171,7 +171,7 @@ ps auxwww | grep -i "screen\|tmux\|byobu" | grep -v grep || true
 echo
 
 echo "USB info"
-lsusb
+lsusb || true
 echo
 
 echo "USB Permissions"
@@ -187,7 +187,7 @@ v4l2-ctl --list-devices || true
 echo
 
 echo "Module info"
-lsmod
+lsmod || true
 echo
 
 
