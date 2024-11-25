b863cf1b258919b958c7c87e36d4c4b1143b0d31
Jürgen Schmied <github@juergenschmied.de>
2024-11-25 19:05:05 +0100

    allow maximum range of 24h for charts

--- indi_allsky/flask/forms.py
+++ indi_allsky/flask/forms.py
@@ -5633,9 +5633,11 @@ class IndiAllskyHistoryForm(FlaskForm):
     HISTORY_SELECT_choices = (
         ('900', '15 Minutes'),
         ('1800', '30 Minutes'),
+        ('2700', '45 Minutes'),
         ('3600', '1 Hour'),
         ('7200', '2 Hours'),
         ('10800', '3 Hours'),
+        ('14400', '4 Hours'),
         ('21600', '6 Hours'),
         ('43200', '12 Hours'),
         ('86400', '24 Hours'),
