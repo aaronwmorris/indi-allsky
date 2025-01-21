0abfa5e6372df2993d4a6da9daa0fd40c97915e5
Aaron W Morris <aaron@aarmor.net>
2025-01-20 20:55:39 -0600

    updates to lightgraph

--- testing/lightgraph.py
+++ testing/lightgraph.py
@@ -21,10 +21,13 @@ logger = logging
 
 class LightGraphGenerator(object):
 
+    graph_height = 50
+    now_size = 8
     light_color = (200, 200, 200)
     dark_color = (15, 15, 15)
-    line_color = (15, 200, 200)
+    line_color = (15, 15, 200)
     border_color = (200, 200, 15)
+    now_color = (15, 200, 15)
 
 
     def __init__(self):
@@ -61,6 +64,10 @@ class LightGraphGenerator(object):
         noon_utc = noon - self.utc_offset
 
 
+        now_offset = int((now - noon).seconds / 60)
+        #logger.info('Now offset: %d', now_offset)
+
+
         lightgraph_list = list()
         for x in range(1440):
             self.obs.date = noon_utc + timedelta(minutes=x)
@@ -83,20 +90,53 @@ class LightGraphGenerator(object):
 
 
         lightgraph = numpy.array([lightgraph_list], dtype=numpy.uint8)
-        lightgraph = cv2.resize(lightgraph, (1440, 50), interpolation=cv2.INTER_AREA)
+        lightgraph = cv2.resize(
+            lightgraph,
+            (1440, self.graph_height),
+            interpolation=cv2.INTER_AREA,
+        )
 
-        for x in range(22):
+
+        # draw hour ticks
+        for x in range(1, 24):
             cv2.line(
                 img=lightgraph,
-                pt1=(60 * (x + 1), 0),
-                pt2=(60 * (x + 1), 50),
+                pt1=(60 * x, 0),
+                pt2=(60 * x, self.graph_height),
                 color=tuple(self.line_color),
                 thickness=1,
                 lineType=cv2.LINE_AA,
             )
 
 
-        lightgraph = cv2.copyMakeBorder(lightgraph, 5, 5, 5, 5, cv2.BORDER_CONSTANT, None, self.border_color)
+        # draw now triangle
+        now_tri = numpy.array([
+            [now_offset - self.now_size, self.graph_height - self.now_size],
+            [now_offset + self.now_size, self.graph_height - self.now_size],
+            [now_offset, self.graph_height]
+        ],
+            dtype=numpy.int32,
+        )
+
+        cv2.fillPoly(
+            img=lightgraph,
+            pts=[now_tri],
+            color=self.now_color,
+        )
+
+
+        # draw border
+        lightgraph = cv2.copyMakeBorder(
+            lightgraph,
+            5,
+            5,
+            5,
+            5,
+            cv2.BORDER_CONSTANT,
+            None,
+            self.border_color,
+        )
+
 
         logger.info(lightgraph.shape)
 
