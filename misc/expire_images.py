55fd62d57318472d79bc3b9715839fb49cb30581
Aaron W Morris <aaron@aarmor.net>
2025-01-31 12:08:11 -0600

    set default image expiration on CLI to 30 days

--- misc/expire_images.py
+++ misc/expire_images.py
@@ -64,7 +64,7 @@ class ExpireImages(object):
         self.config = self._config_obj.config
 
 
-        self._image_days = 10
+        self._image_days = 30
         self._image_raw_days = 10
         self._image_fits_days = 10
         self._video_days = 365
@@ -301,7 +301,7 @@ if __name__ == "__main__":
         '-d',
         help='Images older than days will be deleted',
         type=int,
-        default=10,
+        default=30,
     )
     argparser.add_argument(
         '--raw',
