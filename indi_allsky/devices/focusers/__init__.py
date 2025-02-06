f20d24cb45e4dc4d9865a99018d444c868bd6ccd
Aaron W Morris <aaron@aarmor.net>
2025-02-05 21:39:01 -0600

    use 4th pin on a4988 for sleep mode

--- indi_allsky/devices/focusers/__init__.py
+++ indi_allsky/devices/focusers/__init__.py
@@ -3,8 +3,6 @@ from .focuser_28byj import focuser_28byj_16 as blinka_focuser_28byj_16
 
 from .focuser_a4988 import focuser_a4988_nema17_full as blinka_focuser_a4988_nema17_full
 from .focuser_a4988 import focuser_a4988_nema17_half as blinka_focuser_a4988_nema17_half
-from .focuser_a4988 import focuser_a4988_nema17_quarter as blinka_focuser_a4988_nema17_quarter
-from .focuser_a4988 import focuser_a4988_nema17_eighth as blinka_focuser_a4988_nema17_eighth
 
 from .focuserSerial28byj import FocuserSerial28byj_64 as serial_focuser_28byj_64
 
@@ -13,7 +11,5 @@ __all__ = (
     'blinka_focuser_28byj_16',
     'blinka_focuser_a4988_nema17_full',
     'blinka_focuser_a4988_nema17_half',
-    'blinka_focuser_a4988_nema17_quarter',
-    'blinka_focuser_a4988_nema17_eighth',
     'serial_focuser_28byj_64',
 )
--- indi_allsky/devices/focusers/focuser_a4988.py
+++ indi_allsky/devices/focusers/focuser_a4988.py
@@ -13,6 +13,8 @@ class focuser_a4988(FocuserBase):
     def __init__(self, *args, **kwargs):
         super(focuser_a4988, self).__init__(*args, **kwargs)
 
+        self._sleep = False
+
         pin_names = kwargs['pin_names']
 
         pin1 = getattr(board, pin_names[0])
@@ -24,12 +26,32 @@ class focuser_a4988(FocuserBase):
             'step'    : digitalio.DigitalInOut(pin1),
             'dir'     : digitalio.DigitalInOut(pin2),
             'ms1'     : digitalio.DigitalInOut(pin3),
-            'ms2'     : digitalio.DigitalInOut(pin4),
+            'sleep'   : digitalio.DigitalInOut(pin4),
         }
 
         for label, pin in self.pins.items():
             # set all pins to output
             pin.direction = digitalio.Direction.OUTPUT
+
+
+    @property
+    def sleep(self):
+        return self._sleep
+
+
+    @sleep.setter
+    def sleep(self, new_sleep):
+        new_sleep_b = bool(new_sleep)
+
+
+        if not new_sleep_b:
+            self.pins['sleep'].value = 0
+            time.sleep(0.001)  # wait for 1ms
+        else:
+            self.pins['sleep'].value = 1
+
+
+        self._sleep = new_sleep_b
 
 
     def move(self, direction, degrees):
@@ -50,11 +72,19 @@ class focuser_a4988(FocuserBase):
             self.pins['dir'].value = 0
 
 
+        # disable sleep
+        self.sleep = False
+
+
         for i in range(steps):
             self.pins['step'].value = 1
             time.sleep(0.005)
             self.pins['step'].value = 0
             time.sleep(0.005)
+
+
+        # re-enable sleep
+        self.sleep = True
 
 
 class focuser_a4988_nema17_full(focuser_a4988):
@@ -66,7 +96,6 @@ class focuser_a4988_nema17_full(focuser_a4988):
         super(focuser_a4988_nema17_full, self).__init__(*args, **kwargs)
 
         self.pins['ms1'].value = 0
-        self.pins['ms2'].value = 0
 
 
 class focuser_a4988_nema17_half(focuser_a4988):
@@ -78,29 +107,28 @@ class focuser_a4988_nema17_half(focuser_a4988):
         super(focuser_a4988_nema17_half, self).__init__(*args, **kwargs)
 
         self.pins['ms1'].value = 1
-        self.pins['ms2'].value = 0
 
 
-class focuser_a4988_nema17_quarter(focuser_a4988):
-    # quarter step 0.45 degrees per step
-    STEPS = 800
-
-
-    def __init__(self, *args, **kwargs):
-        super(focuser_a4988_nema17_quarter, self).__init__(*args, **kwargs)
-
-        self.pins['ms1'].value = 0
-        self.pins['ms2'].value = 1
-
-
-class focuser_a4988_nema17_eighth(focuser_a4988):
-    # eighth step 0.225 degrees per step
-    STEPS = 1600
-
-
-    def __init__(self, *args, **kwargs):
-        super(focuser_a4988_nema17_eighth, self).__init__(*args, **kwargs)
-
-        self.pins['ms1'].value = 1
-        self.pins['ms2'].value = 1
+#class focuser_a4988_nema17_quarter(focuser_a4988):
+#    # quarter step 0.45 degrees per step
+#    STEPS = 800
+#
+#
+#    def __init__(self, *args, **kwargs):
+#        super(focuser_a4988_nema17_quarter, self).__init__(*args, **kwargs)
+#
+#        self.pins['ms1'].value = 0
+#        self.pins['ms2'].value = 1
+#
+#
+#class focuser_a4988_nema17_eighth(focuser_a4988):
+#    # eighth step 0.225 degrees per step
+#    STEPS = 1600
+#
+#
+#    def __init__(self, *args, **kwargs):
+#        super(focuser_a4988_nema17_eighth, self).__init__(*args, **kwargs)
+#
+#        self.pins['ms1'].value = 1
+#        self.pins['ms2'].value = 1
 
