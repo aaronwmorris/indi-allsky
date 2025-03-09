954fba51b7ef42eede40edb9c68535b5e89e07a3
Aaron W Morris <aaron@aarmor.net>
2025-03-08 20:40:17 -0600

    catch exception if there are not enough data slots for sensor

--- indi_allsky/sensor.py
+++ indi_allsky/sensor.py
@@ -485,6 +485,8 @@ class SensorWorker(Process):
                 logger.error('Sensor OSError: {0:s}'.format(str(e)))
             except IOError as e:
                 logger.error('Sensor IOError: {0:s}'.format(str(e)))
+            except IndexError as e:
+                logger.error('Sensor slot error: {0:s}'.format(str(e)))
 
 
     def check_dew_heater_thresholds(self):
