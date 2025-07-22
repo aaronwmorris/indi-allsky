bb6bc083ff9f75d3eac923177ac9fad611a09530
Aaron W Morris <aaron@aarmor.net>
2025-07-22 12:24:07 -0500

    add color picker for image circle line

--- indi_allsky/flask/forms.py
+++ indi_allsky/flask/forms.py
@@ -7795,10 +7795,22 @@ class IndiAllskyDriveManagerForm(FlaskForm):
 
 
 class IndiAllskyImageCircleHelperForm(FlaskForm):
+    LINE_COLOR_choices = (
+        ('#00ff00', 'Green'),
+        ('#ff0000', 'Red'),
+        ('#0000ff', 'Blue'),
+        ('#ff00ff', 'Magenta'),
+        ('#ffff00', 'Yellow'),
+        ('#00ffff', 'Cyan'),
+        ('#ffffff', 'White'),
+    )
+
+
     IMAGE_CIRCLE_DIAMETER   = IntegerField('Diameter', render_kw={'step' : '5'})
     OFFSET_X                = IntegerField('X Offset', default=0, render_kw={'step' : '25'})
     OFFSET_Y                = IntegerField('Y Offset', default=0, render_kw={'step' : '25'})
     LINE_WIDTH              = IntegerField('Line', default=5)
+    LINE_COLOR              = SelectField('Line', choices=LINE_COLOR_choices)
 
 
 class IndiAllskyCameraSimulatorForm(FlaskForm):
