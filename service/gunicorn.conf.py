304d02fb945339cff698fd60b1d56dac6345ec57
Aaron W Morris <aaron@aarmor.net>
2025-08-16 20:28:08 -0500

    add gunicorn setting FORWARDED_ALLOW_IPS to fix redirects

--- service/gunicorn.conf.py
+++ service/gunicorn.conf.py
@@ -1,10 +1,13 @@
-worker_class       = 'gthread'
-threads            = 8
-timeout            = 180
-syslog             = True
-syslog_addr        = 'unix:///dev/log'
-syslog_facility    = 'local7'
-loglevel           = 'info'
-reload             = True
-reload_engine      = 'inotify'
-umask              = 0o0022
+worker_class            = 'gthread'
+threads                 = 8
+timeout                 = 180
+syslog                  = True
+syslog_addr             = 'unix:///dev/log'
+syslog_facility         = 'local7'
+loglevel                = 'info'
+reload                  = True
+reload_engine           = 'inotify'
+umask                   = 0o0022
+
+# Not necessary for sockets.  May need to be locked down for a reverse proxy.
+forwarded_allow_ips     = '*'
