3fe902c86e32db4d574b8b247b7d1ad2f6956432
Aaron W Morris <aaron@aarmor.net>
2025-09-17 11:51:18 -0500

    LED disable script and service unit files

---
+++ service/indi-allsky-disable-leds.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+##########################################
+# Script to disable GPIO controlled LEDs #
+##########################################
+
+
+LEDS="
+    ACT
+    PWR
+    led1
+    led0
+    green\:status
+    red\:power
+    librecomputer\:blue
+    librecomputer\:system-status
+    status
+"
+
+echo "Disabling system LEDs"
+
+for LED in $LEDS; do
+    if [ -f "/sys/class/leds/$LED/brightness" ]; then
+        echo "Turning off LED: $LED"
+        echo 0 > "/sys/class/leds/$LED/brightness"
+    fi
+done
