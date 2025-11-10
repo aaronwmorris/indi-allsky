c3f546f49b907f0b957bf8896e890c2e2b094f35
Aaron W Morris <aaron@aarmor.net>
2025-11-10 12:19:27 -0600

    add powerstate string

--- indi_allsky/flask/forms.py
+++ indi_allsky/flask/forms.py
@@ -8049,6 +8049,13 @@ class IndiAllskyNetworkManagerForm(FlaskForm):
         '802-11-wireless': 2,
     }
 
+    nm_powersave_str = {
+        0 : 'Default (Active)',
+        1 : 'Ignore',
+        2 : 'Disable',
+        3 : 'Enable',
+    }
+
 
     def __init__(self, *args, **kwargs):
         super(IndiAllskyNetworkManagerForm, self).__init__(*args, **kwargs)
@@ -8130,7 +8137,7 @@ class IndiAllskyNetworkManagerForm(FlaskForm):
                 conn_dict[settings_uuid]['type'] = '802-3-ethernet'
             elif settings_type == '802-11-wireless':
                 conn_dict[settings_uuid]['type'] = '802-11-wireless'
-                conn_dict[settings_uuid]['powersave'] = int(settings_dict['802-11-wireless'].get('powersave', 0))
+                conn_dict[settings_uuid]['powersave'] = int(settings_dict['802-11-wireless'].get('powersave', -1))
             else:
                 conn_dict[settings_uuid]['type'] = 'other'
 
@@ -8232,17 +8239,23 @@ class IndiAllskyNetworkManagerForm(FlaskForm):
 
 
         for c in filter(lambda item: item[1]['type'] == '802-11-wireless', conn_items_list_sorted):
-            autostart_str = '*'if c[1]['autoconnect'] else ''
+            try:
+                powersave_str = self.nm_powersave_str[c[1]['powersave']]
+            except KeyError:
+                powersave_str = 'UNKNOWN'
+
+            autostart_str = '*' if c[1]['autoconnect'] else ''
+
             conn_select_wifi_list.append((
                 c[0],
-                '{0:s}{1:s} [{2:s}] - {3:s} - {4:s} [prio: {5:d}] [powersave: {6:d}]'.format(
+                '{0:s}{1:s} [{2:s}] - {3:s} - {4:s} [prio: {5:d}] [powersave: {6:s}]'.format(
                     autostart_str,
                     c[1]['id'],
                     ','.join(c[1]['devices']),
                     ','.join(c[1]['addresses']),
                     c[1]['state'],
                     c[1]['autoconnect-priority'],
-                    c[1]['powersave'],
+                    powersave_str,
                 )
             ))
 
