8e48b5b7a49f1a1284167c9e8e395027878ac266
Aaron W Morris <aaron@aarmor.net>
2025-12-06 13:48:00 -0600

    text update

--- setup.sh
+++ setup.sh
@@ -1814,7 +1814,7 @@ if [ ! -d "${ALLSKY_DIRECTORY}/virtualenv/indi-allsky" ]; then
 fi
 
 
-if whiptail --title "Optional Python Modules" --yesno "Would you like to install optional python modules? (Additional database, object storage support)" 0 0 --defaultno; then
+if whiptail --title "Optional Python Modules" --yesno "Would you like to install optional python modules? (Additional database, object storage, YouTube support)" 0 0 --defaultno; then
     OPTIONAL_PYTHON_MODULES=true
 fi
 
