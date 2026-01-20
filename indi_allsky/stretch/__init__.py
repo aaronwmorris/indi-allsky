a2bfa16496a84a9b94a9d68e8473c339566a984b
Ethan Chappel <EthanChappel@users.noreply.github.com>
2026-01-19 18:33:17 -0600

    Fix import.

--- indi_allsky/stretch/__init__.py
+++ indi_allsky/stretch/__init__.py
@@ -1,7 +1,7 @@
 from .mode1_stddev_cutoff import IndiAllSky_Mode1_Stretch as mode1_stddev_cutoff
 from .mode2_mtf import IndiAllSky_Mode2_MTF_Stretch as mode2_mtf
 from .mode2_mtf import IndiAllSky_Mode2_MTF_Stretch_x2 as mode2_mtf_x2
-from .mode3_adaptive_mtf import IndiAllSky_Mode3_Adaptive_MTF_Stretch as mode_adaptive_mtf
+from .mode3_adaptive_mtf import IndiAllSky_Mode3_Adaptive_MTF_Stretch as mode3_adaptive_mtf
 
 
 __all__ = (
