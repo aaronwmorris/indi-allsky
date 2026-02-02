e9e95d9cae5af6017611da2c19b52a41eb68119b
Aaron W Morris <aaron@aarmor.net>
2026-02-02 09:26:46 -0600

    allow 0 value

--- indi_allsky/flask/forms.py
+++ indi_allsky/flask/forms.py
@@ -4150,7 +4150,7 @@ class IndiAllskyConfigForm(FlaskForm):
     EXPOSURE_PERIOD_DAY              = FloatField('Exposure Period (Day)', validators=[DataRequired(), EXPOSURE_PERIOD_DAY_validator])
     CAMERA_SQM__ENABLE               = BooleanField('Enable Camera SQM')
     CAMERA_SQM__EXPOSURE             = FloatField('SQM Exposure', validators=[DataRequired(), CCD_EXPOSURE_validator])
-    CAMERA_SQM__GAIN                 = FloatField('SQM Gain', validators=[DataRequired(), CCD_GAIN_validator])
+    CAMERA_SQM__GAIN                 = FloatField('SQM Gain', validators=[CCD_GAIN_validator])
     CAMERA_SQM__EXPOSURE_PERIOD      = IntegerField('SQM Exposure Period', validators=[DataRequired(), CAMERA_SQM__EXPOSURE_PERIOD_validator])
     FOCUS_MODE                       = BooleanField('Focus Mode')
     FOCUS_DELAY                      = FloatField('Focus Delay', validators=[DataRequired(), FOCUS_DELAY_validator])
