66d4a5dff85a2ab7d5b42638e45c317c915d5b5e
Aaron W Morris <aaron@aarmor.net>
2026-04-07 13:53:12 -0500

    use 32-bit requirements on 32-bit platforms

--- misc/unattended_upgrade.sh
+++ misc/unattended_upgrade.sh
@@ -272,6 +272,7 @@ if [[ "$DISTRO" == "debian_13" ]]; then
         VIRTUALENV_REQ=requirements/requirements_latest_armv6l.txt
         VIRTUALENV_REQ_POST=requirements/requirements_latest_post_32.txt
     elif [ "$CPU_BITS" == "32" ]; then
+        VIRTUALENV_REQ=requirements/requirements_latest_32.txt
         VIRTUALENV_REQ_POST=requirements/requirements_latest_post_32.txt
     fi
 
@@ -363,6 +364,7 @@ elif [[ "$DISTRO" == "debian_12" ]]; then
         VIRTUALENV_REQ=requirements/requirements_latest_armv6l.txt
         VIRTUALENV_REQ_POST=requirements/requirements_latest_post_32.txt
     elif [ "$CPU_BITS" == "32" ]; then
+        VIRTUALENV_REQ=requirements/requirements_latest_32.txt
         VIRTUALENV_REQ_POST=requirements/requirements_latest_post_32.txt
     fi
 
@@ -524,6 +526,7 @@ elif [[ "$DISTRO" == "ubuntu_24.04" ]]; then
         VIRTUALENV_REQ=requirements/requirements_latest_armv6l.txt
         VIRTUALENV_REQ_POST=requirements/requirements_latest_post_32.txt
     elif [ "$CPU_BITS" == "32" ]; then
+        VIRTUALENV_REQ=requirements/requirements_latest_32.txt
         VIRTUALENV_REQ_POST=requirements/requirements_latest_post_32.txt
     fi
 
@@ -605,6 +608,7 @@ elif [[ "$DISTRO" == "ubuntu_22.04" ]]; then
         VIRTUALENV_REQ=requirements/requirements_latest_armv6l.txt
         VIRTUALENV_REQ_POST=requirements/requirements_latest_post_32.txt
     elif [ "$CPU_BITS" == "32" ]; then
+        VIRTUALENV_REQ=requirements/requirements_latest_32.txt
         VIRTUALENV_REQ_POST=requirements/requirements_latest_post_32.txt
     fi
 
