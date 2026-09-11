9407a5d6ef432e67f034b7f48e9fce487da4068e
Hamish West <hamish@hamishwest.xyz>
2026-09-11 14:35:24 +0930

    Remove fallback from MIGRATION_FOLDER and remove old reference in .intall

--- indi_allsky/flask/__init__.py
+++ indi_allsky/flask/__init__.py
@@ -119,10 +119,7 @@ def create_app():
     csrf.exempt(bp_actionapi_allsky)  # disable CSRF for actionapi views
 
     db.init_app(app)
-    migration_dir = app.config.get('MIGRATION_FOLDER', '/usr/share/indi-allsky/migrations')
-    if not os.path.exists(migration_dir) and os.path.exists('/usr/share/indi-allsky/migrations'):
-        migration_dir = '/usr/share/indi-allsky/migrations'
-    migrate.init_app(app, db, directory=migration_dir)
+    migrate.init_app(app, db, directory=app.config['MIGRATION_FOLDER'])
 
 
     login_manager = LoginManager()
