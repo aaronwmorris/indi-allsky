f63ce9dc8521f5a883a292e537076cd32fa04b53
Aaron W Morris <aaron@aarmor.net>
2026-09-17 13:25:58 -0500

    update ubuntu 26.04 package names

--- setup.sh
+++ setup.sh
@@ -1368,7 +1368,7 @@ elif [[ "$DISTRO" == "ubuntu_26.04" ]]; then
         libatlas-ecmwf-dev \
         libimath-dev \
         libopenexr-dev \
-        libgtk-3-0 \
+        libgtk-3-0t64 \
         libssl-dev \
         libxml2-dev \
         libxslt1-dev \
