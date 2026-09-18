2f6b875377434cddafba378e2a31024618b7f231
Aaron W Morris <aaron@aarmor.net>
2026-09-17 22:34:05 -0500

    stream indiserver log based on facility

--- indi_allsky/flask/views.py
+++ indi_allsky/flask/views.py
@@ -10052,8 +10052,9 @@ class StreamLogViewBase(BaseView):
     def __init__(self, **kwargs):
         super(StreamLogViewBase, self).__init__(**kwargs)
 
-        self.user_unit_name = 'changeme'
         self.syslog_facility = None
+        self.user_unit_name = None
+        self.unit_name = None
 
 
     def dispatch_request(self):
@@ -10068,10 +10069,13 @@ class StreamLogViewBase(BaseView):
             if not isinstance(self.syslog_facility, type(None)):
                 app.logger.info('Streaming log from facility %s', self.syslog_facility)
                 reader.add_match(SYSLOG_FACILITY=self.syslog_facility)
-            else:
-                # use unit name
+            elif not isinstance(self.user_unit_name, type(None)):
                 app.logger.info('Streaming log from user unit %s', self.user_unit_name)
                 reader.add_match(_SYSTEMD_USER_UNIT=self.user_unit_name)
+            else:
+                # use unit name
+                app.logger.info('Streaming log from system unit %s', self.unit_name)
+                reader.add_match(_SYSTEMD_UNIT=self.unit_name)
 
 
             reader.seek_tail()
@@ -10107,16 +10111,17 @@ class StreamLogView(StreamLogViewBase):
         super(StreamLogView, self).__init__(**kwargs)
 
         self.syslog_facility = '22'  # local6
-        #self.user_unit_name = app.config['ALLSKY_SERVICE_NAME']
         self.user_unit_name = None
+        self.unit_name = None
 
 
 class StreamIndiserverLogView(StreamLogViewBase):
     def __init__(self, **kwargs):
         super(StreamIndiserverLogView, self).__init__(**kwargs)
 
-        self.syslog_facility = None
-        self.user_unit_name = app.config['INDISERVER_SERVICE_NAME']
+        self.syslog_facility = '21'  # local5
+        self.user_unit_name = None
+        self.unit_name = None
 
 
 class LogDownloadView(BaseView):
